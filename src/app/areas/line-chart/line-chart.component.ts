d8e32f35f59b91899a7c2f5bce5818269fc22901
Storm <stormwitziers123@gmail.com>
2020-06-14 23:44:42 +0200

    fixed linechart

--- src/app/areas/line-chart/line-chart.component.ts
+++ src/app/areas/line-chart/line-chart.component.ts
@@ -1,8 +1,8 @@
 import {Component, EventEmitter, Input, OnInit} from '@angular/core';
-import {ChartDataSets, ChartOptions, ChartType} from "chart.js";
-import {Color, Label} from "ng2-charts";
-import {SprintService} from "../../services/sprint.service";
-import {Sprint} from "../../models/Sprint";
+import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
+import {Color, Label} from 'ng2-charts';
+import {SprintService} from '../../services/sprint.service';
+import {Sprint} from '../../models/Sprint';
 
 @Component({
   selector: 'app-line-chart',
@@ -15,7 +15,7 @@ export class LineChartComponent implements OnInit {
   @Input()
   projectId: string;
   @Input()
-  reload: EventEmitter<void>
+  reload: EventEmitter<void>;
 
   public lineChartData: ChartDataSets[] = [
   ];
@@ -35,20 +35,20 @@ export class LineChartComponent implements OnInit {
 
   async ngOnInit() {
     await this.renderBurndown();
-    this.reload.subscribe(async () => await this.renderBurndown())
+    this.reload.subscribe(async () => await this.renderBurndown());
   }
 
   async renderBurndown(){
     const data = await this.sprintService.getBurnDown(this.projectId, this.sprint.id, this.sprint.start_date.toDate(), this.sprint.end_date.toDate());
 
-    let dates = [];
-    let actualLine = {data: [], label: 'Current stories', fill: false, lineTension: 0 };
-    let optimalLine = { data: [], label: 'Optimal', fill: false, lineTension: 0  };
+    const dates = [];
+    const actualLine = {data: [], label: 'Current stories', fill: false, lineTension: 0 };
+    const optimalLine = { data: [], label: 'Optimal', fill: false, lineTension: 0  };
 
-    for (let date of data) {
-      dates.push(date.date)
-      actualLine.data.push(date.open)
-      optimalLine.data.push(date.optimal)
+    for (const date of data) {
+      dates.push(date.date);
+      actualLine.data.push(date.open);
+      optimalLine.data.push(date.optimal);
     }
 
     this.lineChartLabels = dates;
