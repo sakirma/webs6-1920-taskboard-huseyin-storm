0cbfe8a1782c1b6459f73d41da3ab03c17fac2ba
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2020-06-14 13:23:37 +0200

    added union

--- src/app/services/sprint.service.ts
+++ src/app/services/sprint.service.ts
@@ -70,8 +70,8 @@ export class SprintService {
     await this.firestore
       .collection('projects').doc(projectID)
       .collection('sprints')
-      .doc<Sprint>(sprintID)
-      .update({user_stories: [story.ref]});
+      .doc<any>(sprintID)
+      .update({user_stories: FieldValue.arrayUnion(story.ref)});
 
   }
 
